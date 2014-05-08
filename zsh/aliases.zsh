76e492044a9f03813db92bc1e927b2a9420d46bd
Larry Boltovskoi <larry.bolt@gmail.com>
2014-05-08 09:43:22 +0200

    fuck you oh-my-zsh, zprezto is better

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,5 @@
-alias reload!='. ~/.zshrc'
+alias reload='. ~/.zshrc'
 alias smtp-server='sudo python -m smtpd -n -c DebuggingServer localhost:25'
-alias cdw='cd $HOME/web'
+alias cdw='cd $HOME/web/Projects'
 alias pinguntilback='until ping -W1 -c1 8.8.8.8; do sleep 5; done && say the internet is back'
+alias dotfiles="$EDITOR $DOTS"
---
+++ zsh/misc.zsh
@@ -0,0 +1,6 @@
+alias spacelaunch="play -n -c1 synth whitenoise band -n 100 20 band -n 50 20 gain +30 fade h 1 86400 1"
+alias clear="doge && echo 'use CTRL+L'"
+alias networksound="sudo tcpdump -n -w- | play --buffer 10000 -r 8000 -b 8 -c 1 -e signed-integer -t raw - band 2k vol 0.1"
+
+scp(){ if [[ "$@" =~ : ]];then /usr/bin/scp $@ ; else echo 'You forgot the colon dumbass!'; fi;} # Catch a common scp mistake.
+
