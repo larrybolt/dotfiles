bdc83c255c238ee62b1def6666e7b75af65705ec
Larry Boltovskoi <larry.bolt@gmail.com>
2014-05-08 10:14:43 +0200

    normal stuff, nm

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,14 +8,25 @@ fi
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
+
+# git diff
 alias gd='git diff'
 alias gdt='git difftool -y -t Kaleidoscope'
+
+# git commit
 alias gc='git commit'
 alias gca='git commit -a'
+alias gcm='git commit -m'
+
+# git branches
 alias gco='git checkout'
 alias gb='git branch'
+
+# git status
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
-           perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
+
+# git log
+alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
+alias git-weekoverview='git log --author=$USER --format="- %B" --since=-7days --reverse' #|mail -s "What I've done this week" boss@company\.com
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
