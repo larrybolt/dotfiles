e2f1480f0a77b1fa9fe8663e8261c3224ca87090
Larry Boltovskoi <larry.bolt@gmail.com>
2014-05-09 14:53:53 +0200

    added update command and more

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,28 @@ alias smtp-server='sudo python -m smtpd -n -c DebuggingServer localhost:25'
 alias cdw='cd $HOME/web/Projects'
 alias pinguntilback='until ping -W1 -c1 8.8.8.8; do sleep 5; done && say the internet is back'
 alias dotfiles="$EDITOR $DOTS"
+
+# update all
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm update npm -g; npm update -g; sudo gem update --system; sudo gem update'
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
+alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# Enhanced WHOIS lookups
+alias whois="whois -h whois-servers.net"
+
+
+# View HTTP traffic
+alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
+alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+
+# OS X has no `md5sum`, so use `md5` as a fallback
+command -v md5sum > /dev/null || alias md5sum="md5"
+
+# OS X has no `sha1sum`, so use `shasum` as a fallback
+command -v sha1sum > /dev/null || alias sha1sum="shasum"
+
+# Trim new lines and copy to clipboard
+alias c="tr -d '\n' | pbcopy"
