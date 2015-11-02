e327c43a4a98f936de00a6f4eb901df3f9cab7f4
Larry Boltovskoi <larry.bolt@gmail.com>
2015-11-02 17:25:44 +0100

    trying to make dotfiles less mac-specific

--- vim/aliases.zsh
+++ vim/aliases.zsh
@@ -1,12 +1,12 @@
 #Use the version of vim that comes with MacVim
 #(using an up-to-date version of vim without having to install any packages or replacing the default version)
 # check if MacVim is installed at /Applications or in ~/Application (when using homebrew-cask the latter)
-if [ -e "/Applications/MacVim.app" ]; then
-	alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
-    alias mvim='$DOTS/vim/mvim'
-elif [ -e "$HOME/Applications/MacVim.app" ]; then
-	alias vim='~/Applications/MacVim.app/Contents/MacOS/Vim'
-    alias mvim='$DOTS/vim/mvim'
-fi
-
+#if [ -e "/Applications/MacVim.app" ]; then
+	#alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
+    #alias mvim='$DOTS/vim/mvim'
+#elif [ -e "$HOME/Applications/MacVim.app" ]; then
+	#alias vim='~/Applications/MacVim.app/Contents/MacOS/Vim'
+    #alias mvim='$DOTS/vim/mvim'
+#fi
+#
 export EDITOR=vim
